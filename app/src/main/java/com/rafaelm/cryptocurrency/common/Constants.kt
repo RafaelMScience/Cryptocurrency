107f63f0b1738f619302ef756d9f3dee606a1126
RafaelMScience <rafaelm.science@gmail.com>
2021-09-22 16:25:07 -0400

    feat: add Resource

---
+++ app/src/main/java/com/rafaelm/cryptocurrency/common/Constants.kt
@@ -0,0 +1,4 @@
+package com.rafaelm.cryptocurrency.common
+
+object Constants {
+}
---
+++ app/src/main/java/com/rafaelm/cryptocurrency/common/Resource.kt
@@ -0,0 +1,7 @@
+package com.rafaelm.cryptocurrency.common
+
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Error<T>(message: String?, data: T? = null) : Resource<T>(data, message)
+    class Leading<T>(data: T? = null) : Resource<T>(data)
+}
